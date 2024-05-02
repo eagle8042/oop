db50b06b20c5cb8ae7749a43592237f75b0a7934
Eagle Huang <eagle8042@gmail.com>
2024-05-02 19:59:06 +0800

    Okay

--- get_kshs_news.py
+++ get_kshs_news.py
@@ -1,18 +1,17 @@
 import re
 import urllib.request
 
+class KSHS_Bot:
 
-# 把網站的內容撈回來
-def fetch_kshs_web_content():
-    bot = urllib.request.urlopen('https://www.kshs.kh.edu.tw/view/index.php?WebID=269')
-    content = bot.read().decode('utf8')
-    return content
+    def get_web_content(self):
+        bot = urllib.request.urlopen('https://www.kshs.kh.edu.tw/view/index.php?WebID=269')
+        content = bot.read().decode('utf8')
 
-# 用正規表示式把最新消息的標題撈出來
-def fetch_kshs_news(content):
-    for each_news in re.findall(r'28016.+NowSubId=0">(.+)</a>', content):
-        print(each_news)
+    def fetch_kshs_news(content):
+        for each_news in re.findall(r'28016.+NowSubId=0">(.+)</a>', content):
+            print(each_news)
 
 
-content = fetch_kshs_web_content()
-fetch_kshs_news(content)
+bot = KSHS_Bot
+bot.show_news()
+
